eb6f2252fc655019e780080fff160c3471daccf7
Noe Thalheim <noe@bespinian.io>
2025-02-10 13:42:23 +0100

    Check dynamically for vault

--- nvim/plugin/telekasten.lua
+++ nvim/plugin/telekasten.lua
@@ -1,3 +1,51 @@
+-- Function to get parent directory
+local function get_parent_directory(path)
+  return vim.fn.fnamemodify(path, ':h')
+end
+
+-- Function to get git root directory
+local function get_git_root()
+  local git_dir = vim.fn.FugitiveGitDir()
+  if git_dir then
+    -- Remove the '.git' from the end to get the root
+    return get_parent_directory(git_dir)
+  end
+  return vim.fn.getcwd() -- Fallback to current directory if not in git repo
+end
+
+-- Function to check if directory is valid for a vault
+local function is_valid_vault_directory(dir)
+  -- Add your own validation criteria here
+  -- For example, check if certain folders or files exist
+  local notes_dir = dir .. '/notes'
+  return vim.fn.isdirectory(notes_dir) == 1
+end
+
+-- Function to setup telekasten with dynamic vault
+local function setup_telekasten()
+  local current_dir = get_git_root()
+
+  -- Validate the directory
+  if not is_valid_vault_directory(current_dir) then
+    vim.notify("Current directory contains no valid vault. Checking parent", vim.log.levels.WARN)
+  end
+
+  -- Check the parent directory
+  current_dir = get_parent_directory(current_dir)
+  if not is_valid_vault_directory(current_dir) then
+    vim.notify("Parent directory contains no valid vault. Using ~", vim.log.levels.WARN)
+    current_dir = "~"
+  else
+    vim.notify("Parent directory contains valid vault.", vim.log.levels.INFO)
+  end
+
+  -- Configure telekasten with the current directory
+  require('telekasten').setup({
+    home = current_dir .. '/notes',
+  })
+end
+
+
 -- Launch panel if nothing is typed after <leader>z
 vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
 
@@ -14,6 +62,12 @@ vim.keymap.set("n", "<leader>zv", "<cmd>Telekasten switch_vault<CR>")
 -- Call insert link automatically when we start typing a link
 vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
 
-require('telekasten').setup({
-  home = vim.fn.expand("~/notes"), -- Put the name of your notes directory here
+-- Create an autocommand to update telekasten when directory changes
+vim.api.nvim_create_autocmd("DirChanged", {
+  pattern = "*",
+  callback = function()
+    setup_telekasten()
+  end,
 })
+
+setup_telekasten()
